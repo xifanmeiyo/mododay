f6b4c0713eb09ca9f648367a404eed38f94fa088
PC047\Administrator <1945503330@QQ.COM>
2020-10-23 04:36:43 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,4 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 Route::get('/foo', function () {
     return 'Hello, Welcome to LaravelAcademy.org';
-});
+})->middleware('auth');
